956b3196a232c1f81a5bcbc0153e2a7df8f74938
ryancys1234 <55104016+ryancys1234@users.noreply.github.com>
2022-09-15 16:28:23 -0400

    First commit of the Basics.java file.

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -43,6 +43,7 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
+        System.out.println("Hello World!");
 
 
 
@@ -62,7 +63,7 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
-
+        int my_variable = 100;
 
 
         /* Do not remove the line below: if you did task 2 correctly, then
@@ -99,6 +100,9 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
+        for (int i = 10; i > -1; i--) {
+            System.out.println("Current count: " + i);
+        }
 
 
     }
@@ -143,7 +147,10 @@ public class Basics {
         StringBuilder ret = new StringBuilder();
 
         // Fill in the rest of the body here
-
+        String[] splitWords = to_split.split(" ");
+        for (int i = 0; i < 7; i++) {
+            ret.append(splitWords[i].charAt(0));
+        }
         return ret.toString();
     }
 
@@ -170,6 +177,10 @@ public class Basics {
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
+        if (arr.length < 2) return 0;
+        for (int i = 1; i < arr.length; i += 2) {
+            current_sum += arr[i];
+        }
 
         return current_sum;
     }
